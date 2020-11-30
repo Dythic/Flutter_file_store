5b9e9c75e7175647e0c98cf9ee7c582c8a0d1368
!doia <idoia.reinares@epitech.eu>
2020-11-30 15:56:48 +0100

    [ADD] Login page

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -4,7 +4,128 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("Login"),
+        actions: <Widget>[],
+      ),
+      body: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    String email;
+    String password;
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            height: 54,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextFormField(
+                    onSaved: (String value) {
+                      email = value;
+                    },
+                    validator: (value) {
+                      if (value.isEmpty ||
+                          !value.contains("@") ||
+                          !value.contains(".")) {
+                        return 'Invalid email';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      hintText: "Enter your email",
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            height: 54,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextFormField(
+                    onSaved: (String value) {
+                      password = value;
+                    },
+                    obscureText: true,
+                    validator: (value) {
+                      if (value.isEmpty || value.length < 8) {
+                        return 'Password length must be at least 8';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      hintText: "Enter your password",
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.symmetric(horizontal: 20.0),
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            height: 54,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ButtonTheme(
+                    minWidth: 200.0,
+                    height: 50.0,
+                    child: RaisedButton(
+                      color: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      ),
+                      onPressed: () {
+                        // Validate will return true if the form is valid, or false if
+                        // the form is invalid.
+                        if (_formKey.currentState.validate()) {
+                          _formKey.currentState.save();
+                        }
+                      },
+                      child: Text(
+                        'Login',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
